9af626fc1ec6eadb0b9fa820fb733ec6d8b1bee6
Hmlessard <89013364+Hmlessard@users.noreply.github.com>
2021-09-25 14:32:31 -0500

    return password function

--- script.js
+++ script.js
@@ -1,60 +1,60 @@
 //Available characters
-const available-char = {
-  little-letters: 'abcdefghijklmnopqrstuvwxyz',
-  uppercase: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
-  number: '0123456789',
-  symbol: "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~",
-};
+var lowercase = 'abcdefghijklmnopqrstuvwxyz'
+var uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+var number = '0123456789'
+var symbol = "!@#$%^&*()_-+"
+var generateBtn = document.querySelector("#generate");
+var returnPassword = document.querySelector('#password')
+
 
 function generatePassword() {
+  passwordCharSet = "";
+  
   var characters = "";
-
-  var length = window.prompt("How many characters should the password contain?  Please choose between 8-128");
-
-  var uppercase = window.confirm("Do you want to use uppercase letters?");
-  if (uppercase) {
-    characters += available-char.uppercase;
+//how many characters?
+  var length = prompt("How many characters should the password contain?  Please choose between 8-128");
+  if (length < 8 === true || length > 128 === true) {
+    alert("Please choose between 8-128 characters");
+  }
+  
+  //use else statement to wrap around the other conditions so user does not have to refresh 
+  
+//Upper case?  If yes, include uppercase
+  var isUppercase = confirm("Do you want to use uppercase letters?");
+  if (isUppercase === true) {
+    characters += uppercase;
+  };
+//lowercase?  If yes, include lowercase
+  var isLowercase = confirm("Do you want to use lowercases letters?");
+  if (isLowercase === true) {
+    characters += lowercase;
+  };
+//numbers?  If yes, include numbers
+  var isNumbers = confirm("Do you want to use numbers?");
+  if (isNumbers === true) {
+    characters += number;
+  };
+//symbols?  If yes, include symbols
+  var isSymbols = confirm("Do you want to use symbols?");
+  if (isSymbols === true) {
+    characters += symbol;
   };
 
-  var little-letters = window.confirm("Do you want to use little-letters letters?");
-  if (little-letters) {
-    characters += available-char.little-letters;
-  };
-
-  var numbers = window.confirm("Do you want to use numbers?");
-  if (numbers) {
-    characters += available-char.number;
-  };
-
-  var symbols = window.confirm("Do you want to use symbols?");
-  if (symbols) {
-    characters += available-char.symbol;
-  };
+  console.log(characters);
 
   var password = "";
   for (let i = 0; i < length; i++) {
     password += characters[Math.floor(Math.random() * characters.length)]
   }
+  console.log(password);
+  returnPassword.innerText = password;
   return password;
+
 }
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-   var password = generatePassword();
-   var passwordText = document.querySelector("#password");
-
-   passwordText.value = password;
-
- }
-  
- 
-  
 
 
 // Add event listener to generate button
- generateBtn.addEventListener("click", writePassword);
+ generateBtn.addEventListener("click", generatePassword);
 
-console.log(generatePassword());
+
